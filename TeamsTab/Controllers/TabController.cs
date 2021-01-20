3c24b89e78d92016533160d59004fd79c3440b5d
Clinton_Avery <usa.pants45@gmail.com>
2021-01-20 05:22:16 -0600

    Add project files.

---
+++ TeamsTab/Controllers/TabController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TeamsTab.Models;
+
+namespace TeamsTab.Controllers
+{
+    public class TabController : Controller
+    {
+        private readonly ILogger<TabController> _logger;
+
+        public TabController(ILogger<TabController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Configuration()
+        {
+            return View();
+        }
+
+        public IActionResult Content()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
